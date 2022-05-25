4b0afb42d57216b91e0f20704fd238ade6f115d7
Charalampos Kalligeros <charalampos.kalligeros@trasys.gr>
2022-05-25 23:07:45 +0300

    Adding swagger to backend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,16 +7,25 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+	const app = await NestFactory.create(AppModule);
+	const globalPrefix = 'api';
+	app.setGlobalPrefix(globalPrefix);
+	const port = process.env.PORT || 3333;
+
+	const config = new DocumentBuilder()
+		.setTitle('ToDo List API')
+		.setDescription('This is the API for the ToDo List')
+		.setVersion('1.0')
+		.addTag('TodoList')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(port);
+	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
