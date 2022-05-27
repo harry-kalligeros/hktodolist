618acc0aa819163ce1795f808f741727e323dbe7
Charalampos Kalligeros <charalampos.kalligeros@trasys.gr>
2022-05-27 15:26:33 +0300

    Adding styling and icons to project

---
+++ apps/hktodolist/src/icons/icons.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
+@NgModule({
+	declarations: [],
+	imports: [FontAwesomeModule],
+	exports: [FontAwesomeModule],
+})
+export class IconsModule {}
