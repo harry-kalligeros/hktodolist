448da5ed71f10944c37062451ae1986fd2acce98
Charalampos Kalligeros <charalampos.kalligeros@trasys.gr>
2022-05-29 08:20:20 +0300

    Adding fixtures

---
+++ libs/fixtures/src/todo.fixtures.ts
@@ -0,0 +1,21 @@
+export const todos = [
+	{
+		id: '3f70d660-892b-11ea-ad7e-57f4ce0f7e32',
+		name: 'Dentist',
+		description: 'Tomorrow appointment to my dentist',
+	},
+	{
+		id: 'f6bbec5c-b211-4eda-924d-e0b3920c80b6',
+		name: 'Gym',
+		description: 'Fitness for tonight',
+	},
+];
+
+export const tasks = [
+	{
+		id: 'eb8b0584-3c6e-453b-a327-4757b31c73a9',
+		name: 'task1',
+		description: 'task1 description',
+		todoId: '3f70d660-892b-11ea-ad7e-57f4ce0f7e32',
+	},
+];
